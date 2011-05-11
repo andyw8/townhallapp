e68d3ffe7b7e0b9db3ca22cf053bde81a063e28f
andyw8 <andy@andywaite.com>
2011-05-11 18:45:30 +0100

    some series created during early testing may not have a user associated, so we'll just clear them out to avoid problems

--- db/migrate/20110511173029_add_user_to_series.rb
+++ db/migrate/20110511173029_add_user_to_series.rb
@@ -1,5 +1,11 @@
 class AddUserToSeries < ActiveRecord::Migration
   def self.up
+
+    # some series created during early testing may not have a user associated,
+    # so we'll just clear them out to avoid problems
+
+    Series.delete_all
+
     add_column :series, :user_id, :integer
   end
 
