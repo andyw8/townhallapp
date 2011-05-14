d86ff6688fc9ed6a066bf113707b4263a2f69d10
andyw8 <andy@andywaite.com>
2011-05-14 21:57:14 +0100

    Using RSpec's new 'let' syntax instead of instance vars

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -13,52 +13,52 @@ describe Submission do
 
   context "submission" do
 
-    before { @submission = Factory.create(:submission) }
+    let(:submission) { Factory.create(:submission) }
 
     describe "#user_has_voted?" do
 
       it "should have a default score of 0" do
-        @submission.score.should be(0)
+        submission.score.should be(0)
       end
 
       it "should have a score of 2 for two PLUS votes" do
         2.times do
-          Factory.create(:vote, :vote => 'PLUS', :submission => @submission)
+          Factory.create(:vote, :vote => 'PLUS', :submission => submission)
         end
-        @submission.score.should be(2)
+        submission.score.should be(2)
       end
 
       it "should have a score of -2 for two MINUS votes" do
         2.times do
-          Factory.create(:vote, :vote => 'MINUS', :submission => @submission)
+          Factory.create(:vote, :vote => 'MINUS', :submission => submission)
         end
-        @submission.score.should be(-2)
+        submission.score.should be(-2)
       end
 
       it "should have a net score of 0 for two PLUS votes and two MINUS votes" do
         2.times do
-          Factory.create(:vote, :vote => 'PLUS', :submission => @submission)
-          Factory.create(:vote, :vote => 'MINUS', :submission => @submission)
+          Factory.create(:vote, :vote => 'PLUS', :submission => submission)
+          Factory.create(:vote, :vote => 'MINUS', :submission => submission)
         end
-        @submission.score.should be(0)
+        submission.score.should be(0)
       end
 
       it "should be true if the user has voted on that submission" do
         user = Factory.create(:user)
-        Factory.create(:vote, :submission => @submission, :user => user)
-        @submission.user_has_voted?(user).should be_true
+        Factory.create(:vote, :submission => submission, :user => user)
+        submission.user_has_voted?(user).should be_true
       end
 
       it "should be false if a different user has voted on that submission" do
         user = Factory.create(:user)
         another_user = Factory.create(:user)
-        Factory.create(:vote, :submission => @submission, :user => another_user)
-        @submission.user_has_voted?(user).should be_false
+        Factory.create(:vote, :submission => submission, :user => another_user)
+        submission.user_has_voted?(user).should be_false
       end
 
       it "should be false if the user hasn't voted on that submission" do
         user = Factory.create(:user)
-        @submission.user_has_voted?(user).should be_false
+        submission.user_has_voted?(user).should be_false
       end
 
     end
@@ -66,17 +66,17 @@ describe Submission do
     describe "#allowed_to_vote?" do
 
       it "should be true if the user is signed in and hasn't already voted" do
-        @submission.votes.stub(:find) { nil }
-        @submission.allowed_to_vote?(nil, true).should be_true
+        submission.votes.stub(:find) { nil }
+        submission.allowed_to_vote?(nil, true).should be_true
       end
 
       it "should be false if the user isn't signed in" do
-        @submission.allowed_to_vote?(nil, false).should be_false
+        submission.allowed_to_vote?(nil, false).should be_false
       end
 
       it "should be false if the user has already voted" do
-        @submission.votes.stub(:find) { Factory.build(:vote) }
-        @submission.allowed_to_vote?(nil, true).should be_false
+        submission.votes.stub(:find) { Factory.build(:vote) }
+        submission.allowed_to_vote?(nil, true).should be_false
       end
 
     end
