0fcd8c18d55dac04107589a5dc1a97a75d7295d5
andyw8 <andy@andywaite.com>
2011-05-15 19:37:16 +0100

    Refactoring the specs (not completely happy with them though)

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -15,17 +15,19 @@ describe Submission do
 
     let(:submission) { Submission.new }
 
-    describe "#score" do
+    describe "#calculate_score" do
       it "should handle positive scores" do
-        submission.stub(:plus_votes) { 2 }
-        submission.stub(:minus_votes) { 1 }
+        submission.stub_chain("votes.plus.count").and_return(2)
+        submission.stub_chain("votes.minus.count").and_return(1)
+        submission.should_receive(:save!)
         submission.update_score
         submission.score.should == 1
       end
 
       it "should handle negative scores" do
-        submission.stub(:plus_votes) { 1 }
-        submission.stub(:minus_votes) { 2 }
+        submission.stub_chain("votes.plus.count").and_return(1)
+        submission.stub_chain("votes.minus.count").and_return(2)
+        submission.should_receive(:save!)
         submission.update_score
         submission.score.should == -1
       end
@@ -38,61 +40,58 @@ describe Submission do
       end
 
       it "should return an array of users who voted" do
-        u1 = Factory.create(:user)
-        u2 = Factory.create(:user)
-        Factory.create(:vote, :user => u1, :submission => submission)
-        Factory.create(:vote, :user => u2, :submission => submission)
-        submission.users.should == [u1, u2]
-      end
-
-      it "should not include users who votes on a different submission" do
-        another_submission = Factory.create(:submission)
-        Factory.create(:vote, :submission => another_submission)
-        submission.users.should be_empty
+        user_1, user_2 = User.new, User.new
+        vote_1, vote_2 = Vote.new, Vote.new
+        vote_1.user, vote_2.user = user_1, user_2
+        submission.stub(:votes).and_return([vote_1, vote_2])
+        submission.users.should == [user_1, user_2]
       end
 
     end
 
-    describe "#user_has_voted?" do
+    describe "#score" do
 
       it "should have a default score of 0" do
         submission.score.should be(0)
       end
 
       it "should have a score of 2 for two PLUS votes" do
-        2.times { Factory.create(:plus_vote, :submission => submission) }
+        submission.stub_chain("votes.plus.count").and_return(2)
+        submission.stub_chain("votes.minus.count").and_return(0)
+        submission.should_receive(:save!)
+        submission.update_score
         submission.score.should be(2)
       end
 
       it "should have a score of -2 for two MINUS votes" do
-        2.times { Factory.create(:minus_vote, :submission => submission) }
+        submission.stub_chain("votes.plus.count").and_return(0)
+        submission.stub_chain("votes.minus.count").and_return(2)
+        submission.should_receive(:save!)
+        submission.update_score
         submission.score.should be(-2)
       end
 
       it "should have a net score of 0 for two PLUS votes and two MINUS votes" do
-        2.times do
-          Factory.create(:plus_vote, :submission => submission)
-          Factory.create(:minus_vote, :submission => submission)
-        end
+        submission.stub_chain("votes.plus.count").and_return(0)
+        submission.stub_chain("votes.minus.count").and_return(0)
+        submission.should_receive(:save!)
+        submission.update_score
         submission.score.should be(0)
       end
 
-      it "should be true if the user has voted on that submission" do
-        user = Factory.create(:user)
-        Factory.create(:vote, :submission => submission, :user => user)
-        submission.user_has_voted?(user).should be_true
-      end
+    end
 
-      it "should be false if a different user has voted on that submission" do
-        user = Factory.create(:user)
-        another_user = Factory.create(:user)
-        Factory.create(:vote, :submission => submission, :user => another_user)
-        submission.user_has_voted?(user).should be_false
+    describe "#user_has_voted?" do
+
+      it "should be true if the user has voted on that submission" do
+        submission.stub_chain("votes.find").and_return(Vote.new)
+        submission.user_has_voted?(User.new).should be_true
       end
 
       it "should be false if the user hasn't voted on that submission" do
-        user = Factory.create(:user)
-        submission.user_has_voted?(user).should be_false
+
+        submission.stub_chain("votes.find").and_return(nil)
+        submission.user_has_voted?(User.new).should be_false
       end
 
     end
@@ -100,17 +99,17 @@ describe Submission do
     describe "#allowed_to_vote?" do
 
       it "should be true if the user is signed in and hasn't already voted" do
-        submission.votes.stub(:find) { nil }
-        submission.allowed_to_vote?(nil, true).should be_true
+        submission.votes.stub(:find).and_return(nil)
+        submission.allowed_to_vote?(User.new, true).should be_true
       end
 
       it "should be false if the user isn't signed in" do
-        submission.allowed_to_vote?(nil, false).should be_false
+        submission.allowed_to_vote?(User.new, false).should be_false
       end
 
       it "should be false if the user has already voted" do
-        submission.votes.stub(:find) { Factory.build(:vote) }
-        submission.allowed_to_vote?(nil, true).should be_false
+        submission.votes.stub(:find).and_return(Factory.build(:vote))
+        submission.allowed_to_vote?(User.new, true).should be_false
       end
 
     end
