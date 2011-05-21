0098e17448a9dea363b6cf4ef62cb0b9e9afe27f
andyw8 <andy@andywaite.com>
2011-05-21 10:51:50 +0100

    making use of nulldb

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,13 +2,16 @@ require 'spec_helper'
 
 describe Vote do
 
-  # need to create a record for shoulda's validate_uniqueness_of
-  subject{ Factory.create(:vote) }
-
   it { should validate_presence_of(:user) }
   it { should validate_presence_of(:submission) }
   it { should validate_presence_of(:vote) }
-  it { should validate_uniqueness_of(:user_id).scoped_to(:submission_id) }
+
+  context "", :use_real_database => true do
+    # need to create a record for shoulda's validate_uniqueness_of
+    subject{ Factory.create(:vote) }
+    it { should validate_uniqueness_of(:user_id).scoped_to(:submission_id) }
+  end
+
   it { should belong_to(:submission) }
   it { should belong_to(:user) }
   it { should allow_value('PLUS').for(:vote) }
