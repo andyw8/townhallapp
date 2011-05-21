a0cc0885887d8ad86ea435d0d970b79d0ec6e8a1
andyw8 <andy@andywaite.com>
2011-05-21 10:50:43 +0100

    Allow nulldb to be switched on and off

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'nulldb_rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -24,4 +25,12 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  # this let us switch nulldb on or off per example group
+  config.before(:each) do |example_group|
+    unless example_group.example.metadata[:use_real_database]
+      include NullDB::RSpec::NullifiedDatabase
+    end
+  end
+
 end
