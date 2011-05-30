e89ba4bd8e1e1f5ec8325706e3280938a5a9c201
andyw8 <andy@andywaite.com>
2011-05-30 12:38:24 +0100

    Use stub chain with symbols instead of a string (seems cleaner)

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -14,32 +14,32 @@ describe Submission do
   describe "#update_score" do
 
     it "should set the score to 0 when there are no votes" do
-      submission.stub_chain("votes.plus.count").and_return(0)
-      submission.stub_chain("votes.minus.count").and_return(0)
+      submission.stub_chain(:votes, :plus, :count).and_return(0)
+      submission.stub_chain(:votes, :minus, :count).and_return(0)
       submission.should_receive(:save!)
       submission.update_score
       submission.score.should == 0
     end
 
     it "should set the score by subtracting the MINUS votes from the PLUS votes" do
-      submission.stub_chain("votes.plus.count").and_return(2)
-      submission.stub_chain("votes.minus.count").and_return(1)
+      submission.stub_chain(:votes, :plus, :count).and_return(2)
+      submission.stub_chain(:votes, :minus, :count).and_return(1)
       submission.should_receive(:save!)
       submission.update_score
       submission.score.should == 1
     end
 
     it "should set a negative score when there are more MINUS votes than PLUS votes" do
-      submission.stub_chain("votes.plus.count").and_return(1)
-      submission.stub_chain("votes.minus.count").and_return(2)
+      submission.stub_chain(:votes, :plus, :count).and_return(1)
+      submission.stub_chain(:votes, :minus, :count).and_return(2)
       submission.should_receive(:save!)
       submission.update_score
       submission.score.should == -1
     end
 
     it "should set the score to 0 when there are equal plus and minus votes" do
-      submission.stub_chain("votes.plus.count").and_return(2)
-      submission.stub_chain("votes.minus.count").and_return(2)
+      submission.stub_chain(:votes, :plus, :count).and_return(2)
+      submission.stub_chain(:votes, :minus, :count).and_return(2)
       submission.should_receive(:save!)
       submission.update_score
       submission.score.should == 0
@@ -50,12 +50,12 @@ describe Submission do
   describe "#user_has_voted?" do
 
     it "should return true when there is a vote from that user" do
-      submission.stub_chain("all_votes.find_by_user_id").and_return(mock('Vote'))
+      submission.stub_chain(:all_votes, :find_by_user_id)).and_return(mock('Vote'))
       submission.user_has_voted?(mock('User')).should be_true
     end
 
     it "should return false when there are no votes from that user" do
-      submission.stub_chain("all_votes.find_by_user_id").and_return(nil)
+      submission.stub_chain(:all_votes, :find_by_user_id)).and_return(nil)
       submission.user_has_voted?(mock('User')).should be_false
     end
 
@@ -64,7 +64,7 @@ describe Submission do
   describe "#allowed_to_vote?" do
 
     it "should return true when that user is signed in and hasn't already voted" do
-      submission.stub_chain("all_votes.find_by_user_id").and_return(nil)
+      submission.stub_chain(:all_votes, :find_by_user_id).and_return(nil)
       submission.allowed_to_vote?(mock('User'), true).should be_true
     end
 
@@ -73,7 +73,7 @@ describe Submission do
     end
 
     it "should return false when that user has already voted" do
-      submission.stub_chain("all_votes.find_by_user_id").and_return(mock('Vote'))
+      submission.stub_chain(:all_votes, :find_by_user_id)).and_return(mock('Vote'))
       submission.allowed_to_vote?(mock('User'), true).should be_false
     end
 
@@ -81,17 +81,23 @@ describe Submission do
 
   describe "#author" do
     it "returns the number of email address of the submission creator" do
-      submission.stub_chain("user.email").and_return('hello@example.com')
+      submission.stub_chain(:user, :email).and_return('hello@example.com')
       submission.author.should == 'hello@example.com'
     end
   end
 
   describe "#vote_by_user" do
-    it "return the vote (e.g. PLUS or MINUS) of the given user" do
-      submission.stub_chain("all_votes.find_by_user_id").and_return(mock('Vote', vote: 'PLUS'))
+    it "returns the vote (e.g. PLUS or MINUS) of the given user" do
+      submission.stub_chain(:all_votes, :find_by_user_id)).and_return(mock('Vote', vote: 'PLUS'))
       submission.vote_by_user(mock('User')).should == 'PLUS'
     end
   end
 
+  describe "#comments_count" do
+    it "returns the number of comments on the submission" do
+      submission.stub_chain(:comments, :count).and_return(3)
+      submission.comments_count.should == 3
+    end
+  end
 
 end
