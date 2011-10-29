f74b1457f9875393574928c66277fc57cffdaec8
Andy Waite <andy@andywaite.com>
2011-10-29 14:01:03 +0100

    Diasbling NullDB

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'nulldb_rspec'
+#require 'nulldb_rspec'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
@@ -12,7 +12,7 @@ RSpec.configure do |config|
   # this let us switch nulldb on or off per example group
   config.before(:each) do |example_group|
     unless example_group.example.metadata[:use_real_database]
-      include NullDB::RSpec::NullifiedDatabase
+      #include NullDB::RSpec::NullifiedDatabase
     end
   end
 
