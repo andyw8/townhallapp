8e851b3fd2d41df92f7e667c26d90e366569d839
Andy Waite <andy@andywaite.com>
2011-12-11 12:51:09 +0000

    Terminate Cucumber after 5 five failures

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,6 +1,8 @@
 After do |scenario|
+  @failure_count ||= 0
   if scenario.status == :failed
+    @failure_count += 1
     save_and_open_page
-    Cucumber.wants_to_quit = true
+    Cucumber.wants_to_quit = true if 5 == @failure_count
   end
 end
