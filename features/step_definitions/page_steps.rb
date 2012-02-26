834615d76459d18e64b512bd96529c0b0f2ef019
Andy Waite <andy@andywaite.com>
2012-02-26 21:42:55 +0000

    Removing redundant step defs

--- features/step_definitions/page_steps.rb
+++
@@ -1,7 +0,0 @@
-Then /^the page title should be "([^"]*)"$/ do |title|
-  current_page.page_title.should == title  
-end
-
-Then /^the window title should be "([^"]*)"$/ do |title|
-  current_page.window_title.should == title
-end
